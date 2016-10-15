fedce8d06d0f9560b96827f7e92f426ed567bba8
josue <Jgomez13@terpmail.umd.edu>
2016-10-15 13:06:23 -0400

    update main

--- Twitter/app/src/main/java/com/example/twitter/MainActivity.java
+++ Twitter/app/src/main/java/com/example/twitter/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.twitter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import twitter4j.*;
 
 import java.io.InputStream;
 import java.lang.reflect.Array;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,17 +26,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public Array download_tweets(String screen_name,int number_of_tweets,String  max_id) {
+    public HashMap<String, String> download_tweets(String screen_name, int number_of_tweets, String  max_id) {
         String api_url = "%sstatuses/user_timeline.json?" + BASE_URL;
         api_url += "screen_name="+ screen_name;
         api_url += "count=" +number_of_tweets;
         Twitter twitter = TwitterFactory.getSingleton();
-        List<Status> statuses = twitter.getHomeTimeline();
-        System.out.println("Showing home timeline.");
-        for (Status status : statuses) {
-            System.out.println(status.getUser().getName() + ":" +
-                    status.getText());
+        HashMap<String,String> tweets = null;
+        try {
+            List<Status> statuses = twitter.getHomeTimeline();
+            System.out.println("Showing home timeline.");
+            String curStatus = "";
+            String curUser = "";
+            tweets = new HashMap<>();
+            for (Status status : statuses) {
+                curUser=status.getUser().getName();
+                curStatus = status.getText();
+                tweets.put(curUser,curStatus);
+                System.out.println(curUser+" : "+curStatus);
+            }
+            return tweets;
+        }catch (Exception e ){
+            System.out.println(e.getMessage());
         }
-
+        return tweets;
     }
+
 }
