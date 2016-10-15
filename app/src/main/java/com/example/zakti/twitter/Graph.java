425750e0d7d124aa0bb790c65b0de92d11cbfa2a
Fynel <zaktitus@gmail.com>
2016-10-15 13:04:24 -0400

    adding graph

---
+++ app/src/main/java/com/example/zakti/twitter/Graph.java
@@ -0,0 +1,13 @@
+package com.example.zakti.twitter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by zak on 10/15/2016.
+ */
+
+public class Graph {
+    private ArrayList<String> vertices = new ArrayList();
+    private
+
+}
