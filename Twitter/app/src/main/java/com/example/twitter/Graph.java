388e4d2ea2f4d39405406ad48ea9ef32dbb65aa7
josue <Jgomez13@terpmail.umd.edu>
2016-10-15 14:19:22 -0400

    update main

---
+++ Twitter/app/src/main/java/com/example/twitter/Graph.java
@@ -0,0 +1,21 @@
+package com.example.zakti.twitter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by zak on 10/15/2016.
+ */
+
+public class Graph {
+    private ArrayList<String> vertices = new ArrayList<>();
+    private HashMap<String, Integer> edges = new HashMap<>();
+
+    public boolean add_vertex(String id) {
+        return vertices.add(id);
+    }
+
+    public void add_edge(String from, String to, Integer weight) {
+        String edge = from.
+    }
+
+}
--- Twitter/app/src/main/java/com/example/twitter/MainActivity.java
+++ Twitter/app/src/main/java/com/example/twitter/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.twitter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import twitter4j.*;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 import java.net.*;
 import java.util.ArrayList;
@@ -14,16 +18,79 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     public static final String BASE_URL = "https://api.twitter.com/1.1/";
-
+    private static Twitter twitter;
+    private static int userId;
+    private static AccessToken token;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Consumer Key (API Key)KpQrzxa3mQAKCuoHAoOt9rxHo
+        //Consumer Secret (API Secret)doZz7HbuJUR7PCNEdZ0ZAqnQaMX8n7cLfIKQdf2DQvWiHSbWXE
+        twitter = TwitterFactory.getSingleton();
+        twitter.setOAuthConsumer("[KpQrzxa3mQAKCuoHAoOt9rxHo]", "[doZz7HbuJUR7PCNEdZ0ZAqnQaMX8n7cLfIKQdf2DQvWiHSbWXE]");
+        RequestToken requestToken;
+        try {
+          requestToken = twitter.getOAuthRequestToken();
+        } catch (TwitterException te){
+            te.getStatusCode();
+            requestToken = null;
+        }
 
+        AccessToken accessToken = null;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (null == accessToken) {
+            System.out.println("Open the following URL and grant access to your account:");
+            System.out.println(requestToken.getAuthorizationURL());
+            System.out.print("Enter the PIN(if aviailable) or just hit enter.[PIN]:");
+            try{
+                String  pin;
+                try {
+                     pin = br.readLine();
 
-
-
+                }catch (Exception e){
+                    e.printStackTrace();
+                    pin = "";
+                }
+                if(pin.length() > 0){
+                    accessToken = twitter.getOAuthAccessToken(requestToken, pin);
+                }else{
+                    accessToken = twitter.getOAuthAccessToken();
+                }
+            } catch (TwitterException te) {
+                if(401 ==  te.getStatusCode()){
+                    System.out.println("Unable to get the access token.");
+                }else{
+                    te.printStackTrace();
+                }
+            }
+        }
+        //persist to the accessToken for future reference.
+        try {
+            storeAccessToken((int) twitter.verifyCredentials().getId() , accessToken);
+        }catch (Exception e){
+            e.getMessage();
+        }
+        Status status;
+        try {
+            status= twitter.updateStatus("Status");
+        }catch (TwitterException te){
+            te.getMessage();
+            status=null;
+        }
+        System.out.println("Successfully updated the status to [" + status.getText() + "].");
+        System.exit(0);
     }
+    private static void storeAccessToken(int useId, AccessToken accessToken){
+        //store accessToken.getToken
+        userId =useId;
+        //store accessToken.getTokenSecret()
+        token=accessToken;
+    }
+
+
+
+
 
 
     public HashMap<String, String> download_tweets(String screen_name, int number_of_tweets, String  max_id) {
@@ -32,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
         api_url += "count=" +number_of_tweets;
         Twitter twitter = TwitterFactory.getSingleton();
         HashMap<String,String> tweets = null;
+
         try {
             List<Status> statuses = twitter.getHomeTimeline();
             System.out.println("Showing home timeline.");
@@ -50,4 +118,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return tweets;
     }
+
 }
