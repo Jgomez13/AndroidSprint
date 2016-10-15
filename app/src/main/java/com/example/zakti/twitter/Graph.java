f454b4fc5e7789de0c77ea49c9a831a2e0adc106
Fynel <zaktitus@gmail.com>
2016-10-15 13:18:19 -0400

    graph updates

--- app/src/main/java/com/example/zakti/twitter/Graph.java
+++ app/src/main/java/com/example/zakti/twitter/Graph.java
@@ -1,13 +1,22 @@
 package com.example.zakti.twitter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by zak on 10/15/2016.
  */
 
 public class Graph {
-    private ArrayList<String> vertices = new ArrayList();
-    private
+    private ArrayList<String> vertices = new ArrayList<>();
+    private HashMap<String, Integer> edges = new HashMap<>();
+
+    public boolean add_vertex(String id) {
+        return vertices.add(id);
+    }
+
+    public void add_edge(String from, String to, Integer weight) {
+        String edge = from.
+    }
 
 }
